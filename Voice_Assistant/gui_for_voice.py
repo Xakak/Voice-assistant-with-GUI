b9a66db9fe8586eb2f2bb8bc5e1d8726e5570e32
Rayan <xakak2579@gmail.com>
2023-04-25 13:23:04 +0500

    Initial Commit

---
+++ Voice_Assistant/gui_for_voice.py
@@ -0,0 +1,56 @@
+import tkinter as tk
+import subprocess
+from PIL import ImageTk, Image
+
+#main window
+root=tk.Tk()
+root.title("Virtual Assistant")
+root.geometry("600x750")
+root.minsize(1200,700)
+root.maxsize(1200,700)
+
+#background img:
+img=Image.open("C:\\Users\\admin\\Documents\\Python\\assis.jpg")
+resized_img=img.resize((1200,900))
+img=ImageTk.PhotoImage(resized_img)
+
+bg_canvas=tk.Canvas(root,width=img.width(),height=img.height())
+bg_canvas.create_image(0,0,anchor="nw",image=img)
+bg_canvas.pack()
+
+#text
+text_label=tk.Label(root,text="Virtual Assistant",fg="black",bg="#dddddd")
+text_label.place(x=400,y=50)
+text_label.config(font=("Barlow", 40))
+
+def button_clicked():
+    
+    process = subprocess.Popen(["python.exe","C:\\Users\\admin\\Documents\\Python\\main.py"], stdout=subprocess.PIPE)
+    output, error = process.communicate()
+    output_label.config(text=output.decode(),fg="white")
+    output_label.config(font=("Georgia", 20))
+    output_label.place(x=120,y=590)
+
+#Img for button
+img2=Image.open("C:\\Users\\admin\\Documents\\Python\\recorder.jpg")
+resized_2=img2.resize((80,80))
+img2=ImageTk.PhotoImage(resized_2)
+
+#rectangular
+canvas=tk.Canvas(root,width=1100,height=500,bg="black",highlightcolor="#666362")
+canvas.place(x=60,y=180)
+canvas.create_rectangle(0,0,1100,500,outline="#666362")
+
+#output
+output_label=tk.Label(root,text="Output:",fg="white",bg="black")
+output_label.place(x=70,y=190)
+output_label.config(font=("Georgia", 30))
+
+#button
+
+button=tk.Button(root,image=img2,command= button_clicked)
+button.place(x=1050,y=190)
+
+
+#Always at the end of the code else throws wm error
+root.mainloop()
---
+++ Voice_Assistant/voice assistant.py
@@ -0,0 +1,122 @@
+import speech_recognition as sr
+import pyttsx3
+import time
+import pywhatkit
+import wikipedia
+import pyjokes
+import webbrowser
+import os
+import sys
+import pyautogui
+
+
+
+listener = sr.Recognizer()
+engine = pyttsx3.init()
+voices = engine.getProperty('voices')
+engine.setProperty('voice', voices[1].id)
+
+
+def talk(text):
+    engine.say(text)
+    engine.runAndWait()
+
+
+def take_command():
+   
+        with sr.Microphone() as source:
+            print('Listening...')
+            voice = listener.listen(source)
+            command = listener.recognize_google(voice)
+            command = command.lower()
+            if 'alexa' in command:
+                command = command.replace('alexa', '')
+                print(command)
+            return command
+            
+
+
+def run_alexa():
+    command = take_command()
+    print(command)
+    
+    if 'play' in command:
+        song = command.replace('play', '')
+        print('playing' + song)
+        talk('playing ' + song)
+        pywhatkit.playonyt(song)
+    elif 'open camera' in command:
+        pyautogui.press('win')
+        time.sleep(1)
+        pyautogui.write('camera')
+        time.sleep(1)
+        pyautogui.press('enter')
+        time.sleep(3)
+        pyautogui.press('enter')
+        time.sleep(3)
+        pyautogui.hotkey('alt', 'f4')
+
+    elif 'who is' in command:
+        person = command.replace('who is', '')
+        info = wikipedia.summary(person, 1)
+        print(info)
+        talk(info)
+    elif 'open youtube' in command:
+        webbrowser.open("https://www.youtube.com/")
+    elif 'my location' in command:
+        print("You are somewhere in Cadet College HassanAbdal,Attock District,Pakistan")
+        talk("You are somewhere in Cadet College HassanAbdal,Attock District,Pakistan")
+    elif "sign off" in command:
+        talk(' alright then, I am switching off')
+        print(' alright then, I am switching off')
+        sys.exit()
+    elif 'close google' in command:
+        os.system("taskkill /f /im chrome.exe")
+
+    elif 'open google chrome' in command:
+        webbrowser.open("google.com")
+    elif "sleep" in command:
+        os.system("rundll32.exe powrprof.dll,SetSuspendState 0,1,0")
+    elif 'joke' in command:
+        print(pyjokes.get_joke())
+        talk(pyjokes.get_joke())
+    elif 'google search' in command:
+        query = command.replace("google search", "")
+        pyautogui.hotkey('alt', 'd')
+        pyautogui.write(f"{query}", 0.1)
+        pyautogui.press('enter')
+    elif 'youtube search' in command:
+        query = command.replace("youtube search", "")
+        pyautogui.hotkey('alt', 'd')
+        pyautogui.press('tab')
+        pyautogui.press('tab')
+        pyautogui.press('tab')
+        pyautogui.press('tab')
+        pyautogui.press('tab')
+        pyautogui.press('tab')
+        pyautogui.press('tab')
+        pyautogui.write(f"{query}", 0.1)
+        pyautogui.press('enter')
+    elif 'open new window' in command:
+        pyautogui.hotkey('ctrl', 'n')
+    elif 'open new tab' in command:
+        pyautogui.hotkey('ctrl', 't')
+    elif 'open download' or 'open downloads' in command:
+        pyautogui.hotkey('ctrl', 'j')
+    elif 'previous tab' in command:
+        pyautogui.hotkey('ctrl', 'shift', 'tab')
+    elif 'next tab' in command:
+        pyautogui.hotkey('ctrl', 'tab')
+    elif 'close tab' in command:
+        pyautogui.hotkey('ctrl', 'w')
+    elif 'close window' in command:
+        pyautogui.hotkey('ctrl', 'shift', 'w')
+
+    else:
+        talk('Please say the command again.')
+
+
+if __file__ == "C:\\Users\\admin\\Documents\\Python\\main.py":
+    run_alexa()
+
+
